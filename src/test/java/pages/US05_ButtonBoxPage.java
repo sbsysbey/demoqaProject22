d46acdd29736729297e5b10fd30db7321693e85b
sbsysbey <skoru1972@gmail.com>
2021-10-22 14:54:03 +0200

    US05

--- src/test/java/pages/US05_ButtonBoxPage.java
+++ src/test/java/pages/US05_ButtonBoxPage.java
@@ -14,4 +14,26 @@ public class US05_ButtonBoxPage {
     @FindBy(xpath = "(//li[@id='item-4'])[1]")
     public WebElement button;
 
+    @FindBy(id = "doubleClickBtn")
+    public WebElement doubleClickMe;
+
+    @FindBy(id = "rightClickBtn")
+    public WebElement rightClickMe;
+
+    @FindBy(xpath= "(//button[@class='btn btn-primary'])[3]")
+    public WebElement ClickMe;
+
+    @FindBy(id = "doubleClickMessage")
+    public WebElement doubleClickMessage;
+
+    @FindBy(id = "rightClickMessage")
+    public WebElement rightClickMessage;
+
+    @FindBy(id = "dynamicClickMessage")
+    public WebElement dynamicClickMessage;
+
+    @FindBy(xpath = "//div[@class='main-header']")
+    public WebElement yaziButton;
+
+
 }
--- target/test-classes/features/US05_ButtonBox.feature
+++ target/test-classes/features/US05_ButtonBox.feature
@@ -9,3 +9,14 @@ And Buttons butonuna basiniz
   Scenario: TC_01_DOUBLE_CLICK_ME
     And double click me butonuna basiniz
     And basildigini dogrulayiniz
+
+  Scenario: TC_02_RIGHT_CLICK_ME
+    And right click me butonuna basiniz
+    And right basildigini dogrulayiniz
+
+  Scenario: TC_03_DYNAMIC_CLICK_ME
+    And click me butonuna basiniz
+    And clickme butonuna basildigini dogrulayiniz
+
+    Scenario:TC_04_BUTTONSDISPLAY
+      And Buttons yazisinin gorunurlugunu basildigini Dogrulayiniz
